10ae67809041382e71bfdca3fa9c23dc7b447345
Ya Zhuang <zhuangya@gmail.com>
2019-02-13 18:20:49 +0800

    optimize webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,45 +5,37 @@ const srcDir = './lib/';
 const destDir = 'dist';
 
 module.exports = {
-    entry: {
-        "index": srcDir+"index.ts",
-    },
-    output: {
-        path: path.resolve(__dirname + '/' + destDir),
-        filename: 'index.js',
-        library: 'jest-mock-axios',
-        libraryTarget: 'umd'
-    },
+  entry: {
+    "index": srcDir+"index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname + '/' + destDir),
+    filename: 'index.js',
+    library: 'jest-mock-axios',
+    libraryTarget: 'umd'
+  },
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: [
-                        {
-                            loader: "babel-loader",
-                            options: {
-                                presets: ['@babel/preset-env']
-                            }
-                        },
-                    "ts-loader"
-                ]
-            }
-        ]
-    },
-    externals: {
-        'jest-mock-promise': 'jest-mock-promise'
-    },
-    plugins: [
-        new CleanWebpackPlugin(destDir)
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      }
     ]
+  },
+  externals: {
+    'jest-mock-promise': 'jest-mock-promise'
+  },
+  plugins: [
+    new CleanWebpackPlugin(destDir)
+  ]
 };
