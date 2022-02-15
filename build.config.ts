d141017bc8b95382dcb7aba56ad5d805ac77051b
Jan Beckmann <king-jan1999@hotmail.de>
2022-02-16 00:10:10 +0100

    Remove obsolete build config files

--- build.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineBuildConfig } from "unbuild";
-
-export default defineBuildConfig({
-    entries: [{ input: "lib/index" }],
-    dependencies: ['synchronous-promise'],
-    declaration: true, // generate .d.ts files
-});
